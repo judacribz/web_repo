0cc4a65a21223514cb6417570463679f57d90ba6
judacribz <sheron.balas@gmail.com>
2018-02-14 14:53:34 -0500

    finished lab4 for csci3230

---
+++ csci_3230u/lab/Lab4/lab04.js
@@ -0,0 +1,47 @@
+const SELECT = "selected";
+const DESELECT = "deselected";
+const ROW = "row";
+const COLUMN = "col";
+
+window.onload = function () {
+    deselectAll();
+};
+
+function selectRow(studentNum) {
+    var row = studentNum.getAttribute(ROW);
+    var cells = document.querySelectorAll('[row="' + row + '"]');
+    deselectAll();
+    setAttrForAll(cells, SELECT, 1);
+};
+
+function selectCol(assignment) {
+    var col = assignment.getAttribute(COLUMN);
+    var cells = document.querySelectorAll('[col="' + col + '"]');
+    deselectAll();
+    setAttrForAll(cells, SELECT, 1);
+};
+
+function selectCell(grade) {
+    var row = grade.getAttribute('row');
+    var col = grade.getAttribute('col')
+    var cell = document.querySelector(
+        '[row="' + row + '"][col="' + col + '"]');
+
+    if (cell.getAttribute('class') == SELECT) {
+        cell.setAttribute("class", DESELECT);
+    } else {
+        deselectAll();
+        cell.setAttribute("class", SELECT);
+    }
+}
+
+function deselectAll() {
+    var cells = document.getElementsByTagName("td");
+    setAttrForAll(cells, DESELECT);
+}
+
+function setAttrForAll(cells, attr, i = 0) {
+    for (i; i < cells.length; i++) {
+        cells[i].setAttribute('class', attr);
+    }
+}
