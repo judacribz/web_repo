37dcd6c5038590e6e5601198f239217d7a5b619f
judacribz <sheron.balas@gmail.com>
2018-03-10 18:16:11 -0500

    tune up code

--- csci_3230u/ass/A2/scripts/asmt2.js
+++ csci_3230u/ass/A2/scripts/asmt2.js
@@ -37,16 +37,19 @@ function downloadWeather(lon, lat) {
     addElem(wind, "h2", "", "Wind", "");
 
     $.getJSON(url, function (data) {
-        addElem(temp, "p", "Current", data.current.temp_c, "°C");
-        addElem(temp, "p", "Feels like", data.current.feelslike_c, "°C");
-        addElem(cond, "p", "", data.current.condition.text, "");
-        addElem(cond, "p", "Cloud cover", data.current.cloud, "%");
-        addElem(cond, "p", "Humidity", data.current.humidity, "%");
-        addElem(cond, "p", "Pressure", data.current.pressure_mb, "mB");
-        addElem(wind, "p", "Direction", data.current.wind_dir, "°");
-        addElem(wind, "p", "Speed", data.current.wind_kph, "°km/h");
+        var currData = data.current;
+
+        addElem(temp, "p", "Current", currData.temp_c, "°C");
+        addElem(temp, "p", "Feels like", currData.feelslike_c, "°C");
+        addElem(cond, "p", "", currData.condition.text, "");
+        addElem(cond, "p", "Cloud cover", currData.cloud, "%");
+        addElem(cond, "p", "Humidity", currData.humidity, "%");
+        addElem(cond, "p", "Pressure", currData.pressure_mb, "mB");
+        addElem(wind, "p", "Direction", currData.wind_dir, "°");
+        addElem(wind, "p", "Speed", currData.wind_kph, "°km/h");
     });
 }
+
 function downloadForecast(lon, lat) {
     var url = "http://api.apixu.com/v1/forecast.json?q=" + lat + "," + lon + "&days=7&key=" + KEY_WEATHER;
     var tr;
@@ -65,19 +68,25 @@ function downloadForecast(lon, lat) {
     addElem("tr", "th", "", "Outlook", "");
 
     $.getJSON(url, function (data) {
+        var forecast,
+            forecastDay,
+            forecastCond;
 
         for (var i = 0; i < 7; i++) {
+            forecast = data.forecast.forecastday[i];
+            forecastDay = forecast.day;
+            forecastCond = forecastDay.condition;
+
             tr = $(document.createElement("tr"));
             $("tbody").append(tr);
 
-            addElem(tr, "td", "", data.forecast.forecastday[i].date, "");
+            addElem(tr, "td", "", forecast.date, "");
 
-            tr.append($(document.createElement("img")).attr("src", data.forecast.forecastday[i].day.condition.icon));
-
-            addElem(tr, "td", "", data.forecast.forecastday[i].day.maxtemp_c, "");
-            addElem(tr, "td", "", data.forecast.forecastday[i].day.mintemp_c, "");
-            addElem(tr, "td", "", data.forecast.forecastday[i].day.maxwind_kph, "");
-            addElem(tr, "td", "", data.forecast.forecastday[i].day.condition.text, "");
+            tr.append($(document.createElement("img")).attr("src", forecastCond.icon));
+            addElem(tr, "td", "", forecastDay.maxtemp_c, "");
+            addElem(tr, "td", "", forecastDay.mintemp_c, "");
+            addElem(tr, "td", "", forecastDay.maxwind_kph, "");
+            addElem(tr, "td", "", forecastCond.text, "");
         };
     });
 
