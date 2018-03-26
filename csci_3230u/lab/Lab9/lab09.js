5bb64135ca6bf32386e14a1497d4e71257bdf5d9
judacribz <sheron.balas@gmail.com>
2018-03-25 20:33:37 -0400

    so far one button works, nav one does not

--- csci_3230u/lab/Lab9/lab09.js
+++ csci_3230u/lab/Lab9/lab09.js
@@ -3,7 +3,11 @@ const opn = require('opn');
 const express = require('express');
 const app = express();
 
+const TITLE = "Lab09";
+const USERS = ['admin', 'bsmith', 'rfortier'];
 const INIT_MSG = 'Please enter a username to check';
+const NEW_USER = "That username is available.";
+const USER_EXISTS = "This username already exists. Please try another.";
 
 app.set('port', process.env.PORT || 3000);
 var url = 'http://localhost:' + app.get('port');
@@ -13,7 +17,6 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
 app.use(bodyParser.json());
 
 // configure view engine
@@ -23,20 +26,34 @@ app.set('view engine', 'pug');
 // routes
 app.get('/', function (request, response) {
     response.render('enterUsername', {
-        title: 'Testing',
+        title: TITLE,
         message: INIT_MSG
     });
 });
 
-
 app.post('/checkUsername', function (request, response) {
-    var username = request.body.username;
-    console.log(username);
-    response.render('enterUsername', {
-        title: username,
-        message: username
-    });
+    checkUsername(request, response)
 });
+
+app.get('/checkUsername', function (request, response) {
+    getthis();
+    // request.body.username = request.params[username];
+    checkUsername(request, response)
+});
+
+function checkUsername(req, res) {
+    var username = req.body.username;
+    var msg = NEW_USER;
+
+    if (USERS.indexOf(username.toLowerCase()) > -1) {
+        msg = USER_EXISTS;
+    }
+
+    res.render('enterUsername', {
+        title: TITLE,
+        message: msg
+    });
+}
 
 app.listen(app.get('port'), function () {
     console.log('Web server listening at ' + url);
